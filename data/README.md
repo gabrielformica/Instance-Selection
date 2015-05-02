17d72a2d8746a194ed566fa248c2272f397087e1
Melecio Ponte <pontezambrano@gmail.com>
2015-05-02 17:18:28 -0430

    *Added data (two sets so far) *Implemented oneNN and related functions *Added a main test (test with bank only)

---
+++ data/README.md
@@ -0,0 +1,2 @@
+Data sets for this problem are currently reduced to problem with real domains. The variable to predict (the target) is the last number of every instance.
+
--- src/is.h
+++ src/is.h
@@ -3,9 +3,13 @@
 
 #include <cstdio>
 #include <string>
+#include <cassert>
+#include <cmath>
 #include <vector>
 #include <bitset>
 #define MAX 10000000
+
+using namespace std;
 
 namespace IS {
 
@@ -13,12 +17,19 @@ namespace IS {
 
   };
   
-  class Instance : public std::vector<int> {
-    
+  class Instance : public std::vector<double> {
+    private:
+      double category;
+    public:
+      double getCategory() { return category; }
+      void setCategory(double c) { category = c; }
   }; 
 
   class Problem : public std::vector<Instance> {
-
+    public:
+        Problem();
+        Problem(const Problem& a) : vector<Instance>(a.begin(), a.end()) { };
+        Problem(int N) : std::vector<Instance>(N) {};
   };
 
   typedef std::bitset<MAX> Solution;
@@ -27,6 +38,39 @@ namespace IS {
     public:
       virtual void optimize(const Problem &T, Solution &R) = 0;
   };
+
+  double calc_distance(Instance a, Instance b) {
+      int i;
+      double dist = 0;
+      assert(a.size() == b.size());
+      for(unsigned i = 0; i < a.size(); ++i) {
+          dist += pow((a[i] - b[i]), 2);
+      }
+      return sqrt(dist);
+  }
+
+  Instance find_nearest(Instance a, Problem p) {
+      Problem::iterator it;
+      int min = MAX;
+      Instance ret;
+      for(it = p.begin(); it < p.end(); ++it) {
+          int dist = calc_distance(*it, a);
+          if (dist < min) {
+            min = dist;                  
+            ret = *it;
+          }
+      }
+      return ret;
+  }
+
+  void oneNN(Problem training, Problem* result) {
+      Problem::iterator it;
+      for(it = result->begin(); it != result->end(); ++it) {
+          Instance nearest = find_nearest(*it, training);
+          (*it).setCategory(nearest.getCategory());
+      }
+  }
+
 }
 
 #endif

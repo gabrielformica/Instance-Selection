c7f85b73c20ba9df1579cfd3fe577768dc8f0102
Melecio Ponte <pontezambrano@gmail.com>
2015-05-30 18:34:00 -0430

    Tweaker class

---
+++ src/tweaker.cpp
@@ -0,0 +1,39 @@
+#include "tweaker.h"
+
+void oneRandomFlip::tweak(IS::Solution &S) {
+    std::bitset<MAX> bits = S.getBits();
+    int size = S.getSize();
+    srand(time(NULL));
+    int n = (rand() % bits.count()) + 1;
+    int i, j;
+    for (j = 0, i = 0; j < size && i < n; j++) if (bits.test(j)) i++;
+    assert(bits.test(j-1));
+    bits[j-1] = 0;
+    S.setBits(bits);
+}
+void upToNRandomFlips::tweak(IS::Solution &S) {
+    std::bitset<MAX> bits = S.getBits();
+    int size = S.getSize();
+
+    srand(time(NULL));
+    int t = (rand() % upto) + 1;
+
+    for(unsigned i = 0; i < t; ++i) {
+        int j = (rand() % size) + 1;
+        bits.flip(j);
+    }
+    S.setBits(bits);
+}
+
+void nRandomFlips::tweak(IS::Solution &S) {
+    std::bitset<MAX> bits = S.getBits();
+    int size = S.getSize();
+
+    srand(time(NULL));
+    for(unsigned i = 0; i < n; ++i) {
+        int j = (rand() % size) + 1;
+        bits.flip(j);
+    }
+    S.setBits(bits);
+}
+
---
+++ src/tweaker.h
@@ -0,0 +1,33 @@
+#ifndef TWEAKER_H
+#define TWEAKER_H
+
+#include "is.h"
+
+class Tweaker {
+  public:
+    virtual void tweak(IS::Solution &S) = 0;
+};
+
+class oneRandomFlip : public Tweaker {
+  public:
+    void tweak(IS::Solution &S);
+};
+
+class upToNRandomFlips : public Tweaker {
+  private:
+    int upto;
+  public:
+    upToNRandomFlips(int ut) : upto(ut) { }
+    void tweak(IS::Solution &S);
+};
+
+class nRandomFlips : public Tweaker {
+  private:
+    int n;
+  public:
+    nRandomFlips(int x) : n(x) { }
+    void tweak(IS::Solution &S);
+};
+
+
+#endif
